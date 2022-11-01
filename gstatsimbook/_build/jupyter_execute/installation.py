36dd4b0016a9c16636d48a70ae64418c2bee665a
GatorGlaciology <56602783+GatorGlaciology@users.noreply.github.com>
2022-11-01 14:44:33 -0400

    Add files via upload

---
+++ gstatsimbook/_build/jupyter_execute/installation.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Installation
+# 
+# The package can be installed directly with `pip` or `conda` or from GitHub.
+# 
+# ## Pip
+# 
+# GStatSim can be installed using the `pip` command:
+
+# In[1]:
+
+
+pip install GStatSim
+
+
+# ## Conda
+# 
+# It can also be installed using `conda`:
+
+# In[2]:
+
+
+conda install -c conda-forge GStatSim
+
+
+# ## GitHub
+# 
+# The most recent version can be installed from GitHub via:
+
+# In[3]:
+
+
+git clone git@github.com/GatorGlaciology/GStatSim
+cd scikit-gstat
+pip install -e .
+
